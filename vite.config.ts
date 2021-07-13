fcd41fcaf5f45f8544c40a62fa61d85932490146
zhaoxingming <zhaoxingming@nucarf.com>
2021-07-13 14:53:09 +0800

    feat: 新增index.html例子入口配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,14 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        open: '/examples/index.html',
+    },
     build: {
         target: 'es2015',
         lib: {
             entry: resolve(__dirname, 'src/main.ts'),
-            name: 'index'
-        }
-        // rollupOptions: {
-        //     // 请确保外部化那些你的库中不需要的依赖
-        //     external: ['vue'],
-        //     output: {
-        //         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        //         globals: {
-        //             vue: 'Vue'
-        //         }
-        //     }
-        // }
-    }
+            name: 'index',
+        },
+    },
 });
