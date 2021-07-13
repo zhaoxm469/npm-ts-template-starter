def2ea07d3141c97398bf0c76c6d4465253b85bd
zhaoxingming <zhaoxingming@nucarf.com>
2021-07-13 21:21:09 +0800

    add: 新增本地存储方法封装类

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,58 @@
-class Utils {
-    name: string | undefined;
+type cacheDataType = {
+    [cacheKeyName: string]: string;
+};
 
-    constructor(name: string) {
-        this.name = name;
+export default class AppCache {
+    prefix: string;
+
+    data: cacheDataType = {};
+
+    constructor(prefix: string = 'app') {
+        this.prefix = prefix;
+        this.init();
     }
 
-    static log(value: string) {
-        console.log(value);
+    init(): void {
+        const data = localStorage.getItem(this.prefix);
+        if (data) this.data = JSON.parse(data);
+    }
+
+    get(cacheKeyName: string): boolean | object {
+        const keyName = this.setCacheKeyName(cacheKeyName);
+        if (this.data[keyName]) return JSON.parse(this.data[keyName]);
+        return false;
+    }
+
+    getAll(): boolean | object {
+        if (localStorage.getItem(this.prefix)) {
+            return JSON.parse(localStorage.getItem(this.prefix) as string);
+        }
+        return false;
+    }
+
+    save(cacheKeyName: string, value: object | string | number): void {
+        const keyName = this.setCacheKeyName(cacheKeyName);
+        this.data[keyName] = JSON.stringify(value);
+        localStorage.setItem(this.prefix, JSON.stringify(this.data));
+    }
+
+    setCacheKeyName(cacheKeyName: string): string {
+        return `${this.prefix}-${cacheKeyName}`;
+    }
+
+    clear(cacheKeyName: string): boolean {
+        const keyName = this.setCacheKeyName(cacheKeyName);
+        if (this.data[keyName]) {
+            delete this.data[keyName];
+            localStorage.setItem(this.prefix, JSON.stringify(this.data));
+            return true;
+        }
+
+        return false;
+    }
+
+    clearAll(): void {
+        this.data = {};
+        localStorage.removeItem(this.prefix);
     }
 }
-
-export const getName = () => {};
-
-export default Utils;
