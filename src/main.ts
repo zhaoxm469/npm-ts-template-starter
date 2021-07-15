1403b2f0019bd49a566f50b8a7e0e8f9ea0f3e72
zhaoxingming <zhaoxingming@nucarf.com>
2021-07-15 11:26:26 +0800

    feat: 增加函数注释

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
+/*
+ * @Author: zhaoxingming
+ * @Date: 2021-07-09 11:01:05
+ * @LastEditTime: 2021-07-15 11:16:00
+ * @LastEditors: vscode
+ * @Description: 本地存储方法封装，让开发人员更便捷的使用！
+ *
+ */
+
+import { name } from './person';
+
 type cacheDataType = {
     [cacheKeyName: string]: string;
 };
@@ -7,9 +18,10 @@ export default class AppCache {
 
     data: cacheDataType = {};
 
-    constructor(prefix: string = 'app') {
+    constructor(prefix = 'app') {
         this.prefix = prefix;
         this.init();
+        console.log(name);
     }
 
     init(): void {
@@ -17,29 +29,54 @@ export default class AppCache {
         if (data) this.data = JSON.parse(data);
     }
 
-    get(cacheKeyName: string): boolean | object {
+    /**
+     * @description: 根据key获取本地存储的数据
+     * @param {string} cacheKeyName
+     * @return { cacheDataType|boolean } 有数据返回数据，无数据返回false
+     */
+    get(cacheKeyName: string): boolean | cacheDataType {
         const keyName = this.setCacheKeyName(cacheKeyName);
         if (this.data[keyName]) return JSON.parse(this.data[keyName]);
         return false;
     }
 
-    getAll(): boolean | object {
+    /**
+     * @description:获取全部本地存储数据
+     * @return {  boolean | cacheDataType }
+     */
+    getAll(): boolean | cacheDataType {
         if (localStorage.getItem(this.prefix)) {
             return JSON.parse(localStorage.getItem(this.prefix) as string);
         }
         return false;
     }
 
-    save(cacheKeyName: string, value: object | string | number): void {
+    /**
+     * @description: 存储数据
+     * @param {string} cacheKeyName
+     * @param {cacheDataType} value
+     * @return { void }
+     */
+    save(cacheKeyName: string, value: cacheDataType | string | number): void {
         const keyName = this.setCacheKeyName(cacheKeyName);
         this.data[keyName] = JSON.stringify(value);
         localStorage.setItem(this.prefix, JSON.stringify(this.data));
     }
 
+    /**
+     * @description: 给本地储存的key设置前缀
+     * @param {string} cacheKeyName
+     * @return { string }
+     */
     setCacheKeyName(cacheKeyName: string): string {
         return `${this.prefix}-${cacheKeyName}`;
     }
 
+    /**
+     * @description: 根据指定key 删除本地存储的数据
+     * @param {string} cacheKeyName
+     * @return { boolean }
+     */
     clear(cacheKeyName: string): boolean {
         const keyName = this.setCacheKeyName(cacheKeyName);
         if (this.data[keyName]) {
@@ -51,6 +88,9 @@ export default class AppCache {
         return false;
     }
 
+    /**
+     * @description: 清空所有数据
+     */
     clearAll(): void {
         this.data = {};
         localStorage.removeItem(this.prefix);
---
+++ src/person.ts
@@ -0,0 +1,3 @@
+export const name = '张三';
+export const color = '红色';
+export const age = '17';
