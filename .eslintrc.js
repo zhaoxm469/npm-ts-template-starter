0cb3845f0fff2d85574b91cd5fab6075a6354512
zhaoxingming <zhaoxingming@nucarf.com>
2021-07-15 11:23:45 +0800

    ci: 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,16 @@ module.exports = {
         browser: true,
         es2021: true,
     },
+    settings: {
+        'import/resolver': {
+            'my-awesome-npm-module': { someConfig: value }
+        }
+    },
     extends: [
+        // eslint 推荐规则
+        'eslint:recommended',
+        // ts 推荐规则
+        'plugin:@typescript-eslint/recommended',
         'airbnb-base',
     ],
     parser: '@typescript-eslint/parser',
@@ -17,6 +26,8 @@ module.exports = {
     rules: {
         indent: [2, 4],
         'no-console': 0,
-		'comma-dangle':0
+        'comma-dangle': 0,
+        'import/no-extraneous-dependencies': 0
     },
+
 };
