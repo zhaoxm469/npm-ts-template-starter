8a0f261c618e77225ba4fe20e8e1f19eb0b44bf3
zhaoxingming <zhaoxingming@nucarf.com>
2021-07-20 20:57:23 +0800

    ci: 新增配置 ， 打包输出d.ts 文件

--- rollup.config.build.ts
+++ rollup.config.build.ts
@@ -3,6 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import babel from '@rollup/plugin-babel';
 import { defineConfig } from 'rollup';
+import dts from 'rollup-plugin-dts';
 import path from 'path';
 
 import pkg from './package.json';
@@ -11,36 +12,47 @@ const extensions = ['.js', '.ts'];
 
 const resolve = (...args) => path.resolve(__dirname, ...args);
 
-export default defineConfig({
-    input: resolve('./src/main.ts'),
-    output: [
-        {
-            file: resolve('./', pkg.main),
-            format: 'umd',
-            name: 'AppCache',
-            sourcemap: true
+export default defineConfig([
+    {
+        input: resolve('./src/main.ts'),
+        output: [
+            {
+                file: resolve('./', pkg.main),
+                format: 'umd',
+                name: 'AppCache',
+                sourcemap: true
+            },
+            {
+                file: resolve('./', pkg.module),
+                format: 'es',
+                name: pkg.name,
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            typescript({
+                declarationDir: path.join(__dirname, './dist/'),
+                declaration: true
+            }),
+            nodeResolve({
+                extensions,
+                modulesOnly: true
+            }),
+            babel({
+                babelHelpers: 'bundled',
+                exclude: 'node_modules/**',
+                extensions
+            }),
+            terser()
+        ]
+    },
+    {
+        // 生成 .d.ts 类型声明文件
+        input: resolve('./src/main.ts'),
+        output: {
+            file: resolve('./dist/' + pkg.name + '.d.ts'),
+            format: 'es'
         },
-        {
-            file: resolve('./', pkg.module),
-            format: 'es',
-            name: pkg.name,
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        typescript({
-            declarationDir: path.join(__dirname, './dist/'),
-            declaration: true
-        }),
-        nodeResolve({
-            extensions,
-            modulesOnly: true
-        }),
-        babel({
-            babelHelpers: 'bundled',
-            exclude: 'node_modules/**',
-            extensions
-        }),
-        terser()
-    ]
-});
+        plugins: [dts()]
+    }
+]);
